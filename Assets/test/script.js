2a386de25a9948d230bba62f7171dcd5afe92a14
Th <114317304+t-golv@users.noreply.github.com>
2024-09-30 22:03:00 -0300

    style test

--- Assets/test/script.js
+++ Assets/test/script.js
@@ -1,1 +1,15 @@
-alert("oi");
+async function main_script() {
+  let greeting = document
+    .querySelectorAll(".chat-list-item")[1]
+    .querySelector(".body-text.robot-text.last-text");
+  greeting.style.width = "100%";
+  let markdown = greeting.querySelector(".markdown-body");
+  let btnScript = document.getElementById("markdown-btn");
+  const response = await fetch("http://127.0.0.1:5502/Assets/test/index.html");
+  const html = await response.text();
+  markdown.innerHTML = html;
+  // markdown.appendChild();
+  btnScript.remove();
+}
+
+main_script();
