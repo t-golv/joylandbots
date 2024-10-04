0151f8515f8c951008e1a11b4c70e09711138c0a
Th <114317304+t-golv@users.noreply.github.com>
2024-10-04 02:19:48 -0300

    test

---
+++ storymode/assets/greeting.js
@@ -0,0 +1,38 @@
+async function main_script() {
+  // Get the URL of the currently executing script
+  const scriptUrl = document.currentScript.src;
+
+  // Parse the URL to get parameters
+  const url = new URL(scriptUrl);
+  const dataParam = url.searchParams.get("file");
+
+  // Use the parameter as needed
+  let greeting = document
+    .querySelectorAll(".chat-list-item")[1]
+    .querySelector(".body-text.robot-text.last-text");
+  greeting.style.width = "100%";
+  greeting.style.padding = "0";
+
+  let textBlack = greeting.querySelector(".text-black");
+  if (textBlack) {
+    textBlack.remove();
+  }
+
+  // Fetch the HTML content
+  const response = await fetch(
+    `https://t-golv.github.io/joylandbots/storymode/assets/greeting.html?file=${dataParam}`
+  );
+  const html = await response.text();
+  greeting.innerHTML += html; // Append the fetched HTML
+
+  // Now, change the iframe src if it exists in the fetched content
+  const iframe = greeting.querySelector(".iframe-greeting"); // Adjust selector as needed
+  if (iframe && dataParam) {
+    iframe.src = `https://t-golv.github.io/joylandbots/storymode/${dataParam}/index.html`;
+  } else {
+    console.error("Iframe not found or no file parameter provided.");
+  }
+}
+
+// Execute the script
+main_script();
