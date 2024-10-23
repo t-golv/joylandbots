c90914a2591849ac94456d488c68437d83223876
Th <114317304+t-golv@users.noreply.github.com>
2024-10-23 05:29:21 -0300

    asdasd

--- storymode/assets/greeting.js
+++ storymode/assets/greeting.js
@@ -5,18 +5,19 @@ async function main_script() {
   // Parse the URL to get parameters
   const url = new URL(scriptUrl);
   const dataParam = url.searchParams.get("file");
+  const dataParamHtml = url.searchParams.get("html");
 
   // Use the parameter as needed
-  const greeting = document
-    .querySelectorAll(".chat-list-item")[1]
-    .querySelector(".body-text.robot-text.last-text");
+  const greetingElements = document.querySelectorAll(
+    ".body-text.robot-text.last-text:has(#markdown-btn)"
+  );
+  const greeting = greetingElements[greetingElements.length - 1];
   greeting.style.width = "100%";
   greeting.style.padding = "0";
   const textBlack = greeting.querySelector(".text-black");
   if (textBlack) {
     textBlack.remove();
   }
-
   // Fetch the HTML content
   const response = await fetch(
     `https://t-golv.github.io/joylandbots/storymode/assets/greeting.html?file=${dataParam}`
@@ -25,9 +26,14 @@ async function main_script() {
   greeting.innerHTML += html; // Append the fetched HTML
 
   // Now, change the iframe src if it exists in the fetched content
-  const iframe = greeting.querySelector(".iframe-greeting"); // Adjust selector as needed
+  const iframe = greeting.querySelectorAll(".iframe-greeting"); // Adjust selector as needed
+
   if (iframe && dataParam) {
-    iframe.src = `https://t-golv.github.io/joylandbots/storymode/${dataParam}`;
+    iframe[
+      iframe.length - 1
+    ].src = `https://t-golv.github.io/joylandbots/storymode/${dataParam}/${
+      dataParamHtml ? dataParamHtml : "index"
+    }.html`;
   } else {
     console.error("Iframe not found or no file parameter provided.");
   } // Change the CSS link
@@ -40,7 +46,7 @@ async function main_script() {
     if (response.ok) {
       cssLink.href = `https://t-golv.github.io/joylandbots/storymode/${dataParam}/greetings.css`; // Set the href to the dynamically generated URL
     } else {
-      cssLink.href = `https://t-golv.github.io/joylandbots/storymode/assets/greetings.css`; // Fallback URL
+      // cssLink.href = `https://t-golv.github.io/joylandbots/storymode/assets/greetings.css`; // Fallback URL
     }
   }
   checkAndSetCSSLink();
